c78d0f6ec3726f1c7478c66a65d7c194ea1b29c1
Tyler Beckman <ty@myriation.xyz>
2025-06-20 22:00:52 -0600

    Add nix flake & update rust nightly

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+        rust-overlay = {
+            url = "github:oxalica/rust-overlay";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        crane.url = "github:ipetkov/crane";
+    };
+
+    outputs = { self, nixpkgs, flake-utils, rust-overlay, crane, ... }:
+        flake-utils.lib.eachDefaultSystem (system: let
+            # Initialize nixpkgs
+            pkgs = nixpkgs.legacyPackages.${system};
+            # Setup the rust toolchain
+            rust-bin = rust-overlay.lib.mkRustBin {} pkgs;
+            rust' = (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml);
+            # Setup rust nix packaging
+            craneLib = (crane.mkLib pkgs).overrideToolchain (_: rust');
+            commonArgs = {
+                src = craneLib.cleanCargoSource ./.;
+                strictDeps = true;
+            };
+            cranePackage = craneLib.buildPackage (commonArgs // {
+                cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+            });
+        in {
+            packages = {
+                default = self.packages.${system}.backend;
+                backend = cranePackage;
+            };
+            devShells.default = craneLib.devShell {
+                # Add all build-time dependencies to the environment
+                packages = cranePackage.buildInputs ++ cranePackage.nativeBuildInputs ++ (with pkgs; [
+                    cargo-deny
+                ]);
+            };
+        });
+}
