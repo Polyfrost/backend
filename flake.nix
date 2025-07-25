a8f29a15295d3ca8ebdaf2b3619e1b875fb661d3
Tyler Beckman <ty@myriation.xyz>
2025-07-25 01:05:23 -0600

    Add more to flake checks

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
             inputs.nixpkgs.follows = "nixpkgs";
         };
         crane.url = "github:ipetkov/crane";
+        advisory-db = {
+            url = "github:rustsec/advisory-db";
+            flake = false;
+        };
         treefmt-nix = {
             url = "github:numtide/treefmt-nix";
             inputs.nixpkgs.follows = "nixpkgs";
@@ -20,6 +24,7 @@
             flake-utils,
             rust-overlay,
             crane,
+            advisory-db,
             treefmt-nix,
             ...
         }:
@@ -41,10 +46,13 @@
                     buildInputs = with pkgs; [ openssl ];
                     nativeBuildInputs = with pkgs; [ pkg-config ];
                 };
+                cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+                commonArgsWithDeps = commonArgs // {
+                    inherit cargoArtifacts;
+                };
                 cranePackage = craneLib.buildPackage (
-                    commonArgs
+                    commonArgsWithDeps
                     // {
-                        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
                         meta = {
                             mainProgram = "backend";
                             license = lib.licenses.gpl3Plus;
@@ -61,7 +69,60 @@
                     backend = cranePackage;
                 };
                 formatter = treefmtEval.config.build.wrapper;
-                checks.formatting = treefmtEval.config.build.check self;
+                checks = {
+                    formatting = treefmtEval.config.build.check self;
+                    clippy = craneLib.cargoClippy (
+                        commonArgsWithDeps // { cargoClippyExtraArgs = "--all-targets -- --deny warnings"; }
+                    );
+                    deny =
+                        let
+                            git = ''HOME="$GIT_HOME" git'';
+                            gitInit = ''
+                                ${git} config --global init.defaultBranch "main"
+                                ${git} config --global user.email "example@example.com"
+                                ${git} config --global user.name "John Doe"
+                                ${git} init
+                                ${git} add -A
+                                ${git} commit -m "init"
+                            '';
+                        in
+                        craneLib.cargoDeny (
+                            commonArgs
+                            // {
+                                cargoDenyChecks = "--disable-fetch all";
+                                nativeBuildInputs = [ pkgs.git ];
+                                configurePhase = ''
+                                    runHook preConfigure
+
+                                    DB_PATH="$CARGO_HOME"/advisory-dbs/advisory-db-3157b0e258782691
+                                    mkdir -p "$DB_PATH"
+
+                                    pushd "$DB_PATH"
+
+                                    ln -s ${advisory-db}/{*,.*} .
+                                    GIT_HOME="$(mktemp -d)"
+                                    ${gitInit} # Cargo-deny complains if it isn't a real repo
+
+                                    popd
+
+                                    runHook postConfigure
+                                '';
+                            }
+                        );
+                    udeps = craneLib.mkCargoDerivation (
+                        commonArgsWithDeps
+                        // {
+                            nativeBuildInputs = [ pkgs.cargo-udeps ];
+                            pnameSuffix = "-udeps";
+                            buildPhaseCargoCommand = ''
+                                cargo --offline \
+                                    udeps \
+                                    --all-targets \
+                                    --all-features
+                            '';
+                        }
+                    );
+                };
                 devShells.default = craneLib.devShell {
                     # Add all build-time dependencies to the environment
                     packages =
@@ -69,6 +130,7 @@
                         ++ cranePackage.nativeBuildInputs
                         ++ (with pkgs; [
                             cargo-deny
+                            cargo-udeps
                             evcxr
                             lldb
                             self.formatter.${system}
